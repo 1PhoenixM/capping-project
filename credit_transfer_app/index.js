45623fca79bc869ef85226f1c62f4d94fe514b71
miori <phoenixemere@gmail.com>
2015-10-29 23:38:00 -0400

    Converted the templates and wrapper into Jade

--- credit_transfer_app/index.js
+++ credit_transfer_app/index.js
@@ -8,6 +8,8 @@ var rootAppDirectory = '/admission/transfer/credits/app';
 var conString = "pg://postgres:Candi7@localhost:5432/credit_transfer";
 var client = new pg.Client(conString);
 app.use('/static', express.static(__dirname + '/static'));
+app.use('/admission/transfer/static', express.static(__dirname + '/static'));
+app.use('/admission/transfer/credits/static', express.static(__dirname + '/static'));
 app.use(rootAppDirectory+'/static', express.static(__dirname + '/static'));
 app.set('views',path.join(__dirname, './views'));
 app.set('view engine', 'jade');
@@ -17,23 +19,27 @@ app.use(bodyParser.urlencoded({
   extended: true
 })); 
 
-function wrapPage(pageBody){
+/*function wrapPage(pageBody){
 	pageTop = fs.readFileSync(path.join(__dirname, './html/wrapper') + '/top.html', 'utf-8');	
 	pageBottom = fs.readFileSync(path.join(__dirname, './html/wrapper') + '/bottom.html', 'utf-8');	
 	return page = pageTop + pageBody + pageBottom;
-}
+}*/
 
 //Redirect to the initial page.
 app.get('/', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/credits.html', 'utf-8'));	
-  res.send(pageBody);
+  /*pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/credits.html', 'utf-8'));	
+  res.send(pageBody);*/
+  res.render("credits", {});
 });
 
 //Wireframe #1
 //At current Marist /admission/transfer/credits page, contains a link to the app.
 app.get(rootDirectory, function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/credits.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("credits", {});
+});
+
+app.get(rootAppDirectory, function (req, res) {
+  res.render("startPage", {test:[]});
 });
 
 //Wireframe #2
@@ -41,90 +47,82 @@ app.get(rootDirectory, function (req, res) {
 app.get(rootAppDirectory + '/start', function (req, res) {
  //pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/start.html', 'utf-8'));
  //res.send(pageBody);
- client.connect();
-var query = client.query("SELECT schoolName From Schools;");
-var test = [];
-query.on("row", function (row, result) {
-        result.addRow(row);
-	test[0] = (result.rows[0].schoolname);
-        console.log(test);
-	res.render("start", {test:test});
+	client.connect();
+	var query = client.query("SELECT schoolName From Schools;");
+	var test = [];
+	query.on("row", function (row, result) {
+			result.addRow(row);
+			test[0] = (result.rows[0].schoolname);
+			console.log(test);
+			res.render("startPage", {test:test});
 
-//	for (school in result){
-//	console.log(school.schoolName);
-	
-});
-query.on("end", function (result) {
-//    console.log( JSON.stringify(result.rows, null, "    "));
- 
-   client.end();
-});
+			//	for (school in result){
+			//	console.log(school.schoolName);
+		
+		});
 
-// res.send(pageBody);
- });
+	query.on("end", function (result) {
+			//    console.log( JSON.stringify(result.rows, null, "    "));
+	 
+			client.end();
+		});
+
+	// res.send(pageBody);
+});
 
 //Wireframe #9
 //The user account creation form. Can pass in query parameters here via the req variable, to handle form errors.
 //Redirect user here if account creation fails.
 app.get(rootAppDirectory + '/createAccount', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/createAccount.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("createAccount", {});
 });
 
 //(no wireframe)
 //Shows message indicating user account was successfully created and contains link to /main
 app.get(rootAppDirectory + '/createAccountSuccess', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/createAccountSuccess.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("createAccountSuccess", {});
 });
 
 //Wireframe #3
 //The user account login page, not usable by employees.
 app.get(rootAppDirectory + '/login', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/login.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("login", {});
 });
 
 //Wireframe #4
 //The "forgot password" page allowing an email to be sent to the user.
 app.get(rootAppDirectory + '/forgotPassword', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/forgotPassword.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("forgotPassword", {});
 });
 
 //Wireframe #5
 //Shows message indicating that a password recovery email was successfully sent.
 app.get(rootAppDirectory + '/forgotPasswordEmailSent', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/forgotPasswordEmailSent.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("forgotPasswordEmailSent", {});
 });
 
 //Wireframe #6
 //The main menu for registered users.
 app.get(rootAppDirectory + '/main', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/main.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("main", {});
 });
 
 //Wireframe #7
 //Page allowing user to change their password.
 app.get(rootAppDirectory + '/changePassword', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/changePassword.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("changePassword", {});
 });
 
 //Wireframe #8
 //Shows message indicating that password was successfully changed.
 app.get(rootAppDirectory + '/changePasswordSuccess', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/changePasswordSuccess.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("changePasswordSuccess", {});
 });
 
 //Wireframe #11
 //Allows user to select courses and update their existing course list.
 app.get(rootAppDirectory + '/courseSelection', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/courseSelection.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("courseSelection", {});
 });
 
 //Wireframe #12
@@ -132,22 +130,19 @@ app.get(rootAppDirectory + '/courseSelection', function (req, res) {
 //If user entered only courses and selected no major, shows their personal percentage completion for all Marist majors in order, based on courses entered.
 //If user entered both courses and selected a major, shows a credit evaluation for that particular major with those courses.
 app.get(rootAppDirectory + '/courseEvaluation', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/courseEvaluation.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("courseEvaluation", {});
 });
 
 //Wireframe #10
 //Form allowing a user to request a course not found in our database.
 app.get(rootAppDirectory + '/requestCourse', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/requestCourse.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("requestCourse", {});
 });
 
 //Wireframe #13
 //Shows message indicating that the course was requested and an email will be sent when a decision has been reached.
 app.get(rootAppDirectory + '/requestCourseConfirmation', function (req, res) {
-  pageBody = wrapPage(fs.readFileSync(path.join(__dirname, './html') + '/requestCourseConfirmation.html', 'utf-8'));	
-  res.send(pageBody);
+  res.render("requestCourseConfirmation", {});
 });
 
 //To be added: Administrator action pages for Admissions/Registrar to alter the database.
@@ -155,6 +150,10 @@ app.get(rootAppDirectory + '/requestCourseConfirmation', function (req, res) {
 //Route to post the account data to (accessed as an object)
 app.post(rootAppDirectory + '/createAccount', function (req, res){
 	console.log(req.body);
+});
+
+app.use(function(req, res, next) {
+    res.render("404", {});
 });
 
 var server = app.listen(8080, function () {
